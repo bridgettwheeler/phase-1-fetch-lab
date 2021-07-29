af24114872f25f406edbc3413c89240f138f0c1a
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-07-28 23:14:38 -0400

    commit tree 3173de1380a27bb216a684c9f510e71fa214edcf

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(jsonobject => renderBooks(jsonobject));
+// .then(function(response) {
+//   response.json();
+// })
+// .then(function(json){
+//   renderBooks(json);
+// })
   
 }
 
